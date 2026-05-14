724c3e62f0c2b9c21e2b727447c0dd0bf4d54cd9
Laura Daniela Merchan Gil <lmercha78@gmail.com>
2026-05-13 21:22:44 -0500

    Prepare app for Render: use PORT, add requirements and Procfile gunicorn

--- app.py
+++ app.py
@@ -132,4 +132,7 @@ def clear_completed():
 # EJECUTAR
 if __name__ == '__main__':
 
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
+
+    app.run(host='0.0.0.0', port=port, debug=debug)
